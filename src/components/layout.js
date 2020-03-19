f1513d67d7b908dc7cf2ad9700e3cb59d0cc6e41
W. Clark Newell <clark@clarknewell.tech>
2020-03-19 16:06:50 -0600

    Clean up on master branch
    Clean up on master branch

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Helmet from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
@@ -8,29 +7,9 @@ import Footer from './footer'
 import './layout.css'
 
 const Layout = ({ children }) => {
-    const data = useStaticQuery(graphql`
-        query SiteMetaQuery {
-            site {
-                siteMetadata {
-                    title
-                    description
-                    keywords
-                }
-            }
-        }
-    `)
-
+    
     return (
         <div>
-            <Helmet
-                title={data.site.siteMetadata.title}
-                meta={[
-                    { name: 'description', content: data.site.siteMetadata.description },
-                    { name: 'keywords', content: data.site.siteMetadata.keywords },
-                ]}
-            >
-            <html lang="en" />
-            </Helmet>
             <Header />
             <RecentPosts />
             <Footer />
